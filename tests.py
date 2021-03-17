9b4f3b249f2c80d76f17dd0b17aa75fbe0506047
natasha <igum.natasha@gmail.com>
2021-03-17 17:01:13 +0300

    Fix tests and add new test

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class Tests:
@@ -13,22 +15,31 @@ class Tests:
         options.add_argument("--disable-infobars")
         self.driver = webdriver.Chrome(".\\chromedriver.exe", options=options)
         self.driver.wait = WebDriverWait(self.driver, 5)
-        #self.driver.refresh()
+        self.driver.get("http://tictactoe.no/")
+        menu = self.driver. \
+            find_elements_by_css_selector('.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73')
+        for e in menu:
+            if e.text == "SETTINGS":
+                e.click()
+
+        dark_style = self.driver.find_element_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1awozwy.r-vl818t.r-pm2fo."
+                                                              "r-gxnn5r.r-ou6ah9.r-rs99b7.r-1loqt21.r-ur6pnr."
+                                                              "r-1777fci.r-crgep1.r-1udh08x.r-bnwqim.r-1otgn73."
+                                                              "r-1mhb1uw")
+        dark_style.click()
+        self.driver.refresh()
 
     def teardown_method(self):
         self.driver.refresh()
         self.driver.close()
 
     def test_multiplayer(self):
-        self.driver.get("http://tictactoe.no/")
         buttom_multiplayer = self.driver.find_element_by_css_selector('.css-18t94o4.css-1dbjc4n.'
                                                                       'r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73')
         buttom_multiplayer.click()
-        self.driver.refresh()
         assert True
 
     def test_number_of_3_cells(self):
-        self.driver.get("http://tictactoe.no/")
         menu = self.driver. \
             find_elements_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73")
         for e in menu:
@@ -39,11 +50,9 @@ class Tests:
                                                                 "r-crgep1.r-1udh08x.r-bnwqim.r-1otgn73.r-1mhb1uw")
         buttom_num_3.click()
         cells = self.driver.find_elements_by_css_selector(".css-1dbjc4n.r-1awozwy.r-13awgt0.r-1777fci")
-        self.driver.refresh()
-        assert 3**2 == len(cells)-5
+        assert 3 ** 2 == len(cells) - 5
 
     def test_number_of_4_cells(self):
-        self.driver.get("http://tictactoe.no/")
         menu = self.driver. \
             find_elements_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73")
         for e in menu:
@@ -54,11 +63,9 @@ class Tests:
                                                                 "r-crgep1.r-1udh08x.r-bnwqim.r-1otgn73.r-1mhb1uw")
         buttom_num_4.click()
         cells = self.driver.find_elements_by_css_selector(".css-1dbjc4n.r-1awozwy.r-13awgt0.r-1777fci")
-        self.driver.refresh()
-        assert 4**2 == len(cells)-5
+        assert 4 ** 2 == len(cells) - 5
 
     def test_number_of_5_cells(self):
-        self.driver.get("http://tictactoe.no/")
         menu = self.driver. \
             find_elements_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73")
         for e in menu:
@@ -70,28 +77,47 @@ class Tests:
                                                                 "r-1mhb1uw")
         buttom_num_5.click()
         cells = self.driver.find_elements_by_css_selector(".css-1dbjc4n.r-1awozwy.r-13awgt0.r-1777fci")
-        self.driver.refresh()
-        assert 5**2 == len(cells)-5
+        assert 5 ** 2 == len(cells) - 5
+
+    def test_enter_lobby_id(self):
+        menu = self.driver. \
+            find_elements_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73")
+        for e in menu:
+            if e.text == "ONLINE MULTIPLAYER":
+                e.click()
+        lobby_id = self.driver.find_element_by_css_selector("input.css-1cwyjr8.r-1wk2t95.r-f8cu29.r-1f0042m."
+                                                             "r-jwli3a.r-adyw6z.r-eu3ka.r-xyro26.r-zt59i6."
+                                                             "r-q4m81j.r-l0gwng")
+        lobby_id.click()
+        lobby_id.clear()
+        lobby_id.send_keys('ddd')
+        lobby_id.send_keys(Keys.RETURN)
+        join = self.driver.find_elements_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim."
+                                                         "r-1otgn73")
+        for e in join:
+            if e.text == "JOIN":
+                e.click()
+        time.sleep(10)
+        answer = self.driver.find_element_by_css_selector(".css-901oao.r-1erp77z.r-a023e6.r-vw2c0b.r-1tmtcfd.r-q4m81j")
+        assert 'This lobby does not exist...' == answer.text
 
     def test_change_style_(self):
-        self.driver.get("http://tictactoe.no/")
         menu = self.driver. \
             find_elements_by_css_selector('.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73')
         for e in menu:
             if e.text == "SETTINGS":
                 e.click()
 
-        first_style = self.driver.find_element_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1awozwy.r-14lw9ot.r-zmljjp."
+        first_style = self.driver.find_element_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1awozwy.r-fnzcxi.r-zmljjp."
                                                                "r-t12b5v.r-rs99b7.r-1loqt21.r-ur6pnr.r-1777fci.r-crgep1"
                                                                ".r-1udh08x.r-bnwqim.r-1otgn73.r-1mhb1uw")
         first_style.click()
-        background = self.driver.find_element_by_css_selector(".css-1dbjc4n.r-1awozwy.r-blqegh.r-13awgt0.r-1777fci")
-        assert background.value_of_css_property("background-color") == "rgba(42, 45, 52, 1)"
-        self.driver.refresh()
+
+        background = self.driver.find_element_by_css_selector(".css-1dbjc4n.r-1awozwy.r-1ji381s.r-13awgt0.r-1777fci")
+        assert background.value_of_css_property("background-color") == "rgba(230, 234, 235, 1)"
 
     def test_change_style_light(self):
-        self.driver.get("http://tictactoe.no/")
-        menu = self.driver.\
+        menu = self.driver. \
             find_elements_by_css_selector('.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73')
         for e in menu:
             if e.text == "SETTINGS":
@@ -103,22 +129,18 @@ class Tests:
         light_style.click()
         background = self.driver.find_element_by_css_selector(".css-1dbjc4n.r-1awozwy.r-1ji381s.r-13awgt0.r-1777fci")
         assert background.value_of_css_property("background-color") == "rgba(230, 234, 235, 1)"
-        self.driver.refresh()
 
     def test_change_style_dark(self):
-        self.driver.get("http://tictactoe.no/")
         menu = self.driver. \
             find_elements_by_css_selector('.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1udh08x.r-bnwqim.r-1otgn73')
         for e in menu:
             if e.text == "SETTINGS":
                 e.click()
 
-        dark_style = self.driver.find_element_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1awozwy.r-fnzcxi.r-pm2fo."
-                                                               "r-gxnn5r.r-ou6ah9.r-rs99b7.r-1loqt21.r-ur6pnr."
-                                                               "r-1777fci.r-crgep1.r-1udh08x.r-bnwqim.r-1otgn73."
-                                                               "r-1mhb1uw")
+        dark_style = self.driver.find_element_by_css_selector(".css-18t94o4.css-1dbjc4n.r-1awozwy.r-14lw9ot.r-pm2fo."
+                                                              "r-gxnn5r.r-ou6ah9.r-rs99b7.r-1loqt21.r-ur6pnr."
+                                                              "r-1777fci.r-crgep1.r-1udh08x.r-bnwqim.r-1otgn73."
+                                                              "r-1mhb1uw")
         dark_style.click()
-        self.driver.refresh()
         background = self.driver.find_element_by_css_selector(".css-1dbjc4n.r-1awozwy.r-blqegh.r-13awgt0.r-1777fci")
         assert background.value_of_css_property("background-color") == "rgba(42, 45, 52, 1)"
-
